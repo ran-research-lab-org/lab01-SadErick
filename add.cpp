210564625193228d9b7eefa4de9ae018dd599b1e
SadErick <saderick8@gmail.com>
2024-08-23 16:22:39 +0000

    Added add.cpp + added lab01.cpp

---
+++ add.cpp
@@ -0,0 +1,14 @@
+// Saderick I. Gonzalez Soler, SadErick
+// This program adds 2 numbers
+
+#include <iostream>
+
+int main() {
+  int number1, number2, sum;
+  number1 = 12;
+  number2 = 13;
+  sum = number1 + number2;
+  std::cout << "The sum of these two integers is "
+            << sum << std::endl;
+  return 0;
+}
